870686b7abd03f5596c2328f897379b601011652
Sander Revenberg <sander@revenberg.info>
2021-01-07 10:46:35 +0100

    .

--- README.md
+++ README.md
@@ -3,5 +3,3 @@ wget https://raw.githubusercontent.com/Revenberg/ansible_zigbee/master/install.s
 
 Once Node-RED is running you can access the editor in a browser.
 If you are using the browser on the Pi desktop, you can open the address: http://localhost:1880.
-
-Start platypush (either by running platypush or starting the platypush.service systemd service).Open http://host-or-ip:8008/ in a browser.
--- install.sh
+++ install.sh
@@ -39,23 +39,17 @@ sudo ssh-keygen -l -f /etc/ssh/ssh_host_rsa_key
 
 pswrd=$(cat /home/pi/.pswrd)
 
-echo "all:" > /home/pi/ansible/hosts
-echo "  vars:" >> /home/pi/ansible/hosts
-echo "    ansible_connection: ssh" >> /home/pi/ansible/hosts
-echo "    ansible_ssh_user: pi" >> /home/pi/ansible/hosts
-ansible-vault encrypt_string --vault-password-file /home/pi/.pswrd $pswrd --name '    ansible_ssh_pass'  >> /home/pi/ansible/hosts
-echo "rpi:" >> /home/pi/ansible/hosts
-echo "  hosts:" >> /home/pi/ansible/hosts
-
+echo "" > /home/pi/ansible/hosts
+echo "ansible_connection: ssh" >> /home/pi/ansible/hosts
+echo "ansible_ssh_user: pi" >> /home/pi/ansible/hosts
+ansible-vault encrypt_string --vault-password-file /home/pi/.pswrd $pswrd --name 'ansible_ssh_pass'  >> /home/pi/ansible/hosts
+echo "" >> /home/pi/ansible/hosts
+echo "[rpi]" >> /home/pi/ansible/hosts
 
 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'  | head -n 1 | while read line;
 do
-  echo "    $line:" >> /home/pi/ansible/hosts
-  echo "      ansible_user: pi" >> /home/pi/ansible/hosts
+  echo "home ansible_host=$line ansible_connection=ssh ansible_user: pi" >> /home/pi/ansible/hosts
 done
-
-#echo /home/pi/ansible/hosts
-#ansible-vault encrypt_string --vault-password-file /home/pi/.pswrd '$pswrd' --name ' ansible_ssh_pass'  >> /home/pi/ansible/hosts
 
 cd /home/pi/ansible_zigbee
 git pull
