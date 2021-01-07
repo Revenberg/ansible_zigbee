31627eb4f882dbca4ab7b91f790e9dd7333923d0
Sander Revenberg <sander@revenberg.info>
2021-01-07 13:31:33 +0100

    .

--- install.sh
+++ install.sh
@@ -42,13 +42,13 @@ pswrd=$(cat /home/pi/.pswrd)
 echo "" > /home/pi/ansible/hosts
 echo "ansible_connection=ssh" >> /home/pi/ansible/hosts
 echo "ansible_ssh_user=pi" >> /home/pi/ansible/hosts
-echo "ansible_ssh_pass="$(ansible-vault encrypt_string --vault-password-file /home/pi/.pswrd $pswrd)  >> /home/pi/ansible/hosts                          
+#echo "ansible_ssh_pass="$(ansible-vault encrypt_string --vault-password-file /home/pi/.pswrd $pswrd)  >> /home/pi/ansible/hosts                          
 echo "" >> /home/pi/ansible/hosts
 echo "[rpi]" >> /home/pi/ansible/hosts
 
 ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'  | head -n 1 | while read line;
 do
-  echo "home ansible_host=$line ansible_connection=ssh ansible_user=pi" >> /home/pi/ansible/hosts
+  echo "home ansible_host=$line ansible_connection=local ansible_user=pi" >> /home/pi/ansible/hosts
 done
 
 cd /home/pi/ansible_zigbee
