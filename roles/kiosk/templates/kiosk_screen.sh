f8fd0625b45fa5560b5109ee357416556c9dff54
Sander Revenberg <sander@revenberg.info>
2021-01-27 20:19:37 +0100

    .

--- roles/kiosk/templates/kiosk_screen.sh
+++ roles/kiosk/templates/kiosk_screen.sh
@@ -8,7 +8,7 @@
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://{{ ansible_ssh_host }} &
+/usr/bin/chromium-browser --noerrdialogs --disable-infobars --kiosk http://{{ ansible_ssh_host }} 192.168.2.99 &
 
 while true; do
    xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
