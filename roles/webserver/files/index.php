4ed6c54c31f1c3ed70f04583b32e0f6b7dba13ff
Revenberg <sander@revenberg.info>
2021-01-29 13:14:21 +0100

    Create index.php

---
+++ roles/webserver/files/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="nl">
+<head>
+<META charset="UTF-8">
+<title>Weer 1</title>
+</head>
+<body>
+
+<table width="166%" height="100%"><tr>
+<td width="33%"><iframe frameborder="0" scrolling="no" class="weerplazaWidget" src="https://www.weerplaza.nl/weerwidgets/locatie/18565/" style="padding: 10px; width: 100% !important; max-width: 370px !important; height: 350px !important; border: 1px solid rgb(244, 244, 244) !important; box-sizing: border-box !important;"></iframe></td>
+<td width="33%">...iframe...</td>
+</tr></table>
+</body>
+</html>
