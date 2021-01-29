4c9c4851eac31a8c79bbb10d4a23cb618bfb5d6f
Revenberg <sander@revenberg.info>
2021-01-29 10:47:42 +0100

    Update kiosk_screen.sh

--- roles/kiosk/templates/kiosk_screen.sh
+++ roles/kiosk/templates/kiosk_screen.sh
@@ -8,7 +8,7 @@
 sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --force-device-scale-factor=0.5 --noerrdialogs --disable-infobars --window-size=480,320 --kiosk http://{{ ansible_ssh_host }} http://192.168.2.99 &
+/usr/bin/chromium-browser --force-device-scale-factor=0.5 --noerrdialogs --disable-infobars --window-size=480,320 --kiosk http://{{ ansible_ssh_host }} http://{{ ansible_ssh_host }}:8080/weer2.html http://{{ ansible_ssh_host }}:8080/weer3.html http://{{ ansible_ssh_host }}:8080/weer4.htlm &
 
 while true; do
    xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
