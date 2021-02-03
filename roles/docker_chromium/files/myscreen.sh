68cd57daec368c933fcf835433554dd616f99ae6
Sander Revenberg <sander@revenberg.info>
2021-02-03 21:18:22 +0100

    .

--- roles/docker_chromium/files/myscreen.sh
+++ roles/docker_chromium/files/myscreen.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-@xset s noblank
-@xset s off
-@xset -dpms
+#xset s noblank
+#xset s off
+#xset -dpms
 
 #unclutter -idle 0.5 -root &
 export DISPLAY=:0.0
 
-sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
-sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
+#sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
+#sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-/usr/bin/chromium-browser --force-device-scale-factor=0.5 --noerrdialogs --disable-infobars --window-size=480,320 --kiosk http://127.0.0.1 http://127.0.0.1:8080/index.html  &
+chromium-browser --force-device-scale-factor=0.5 --noerrdialogs --disable-infobars --window-size=480,320 --kiosk http://127.0.0.1:1880/ui  &
 
 while true; do
    xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
