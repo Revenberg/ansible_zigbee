e2ef43265b7ed72ef10e66a6f83591f0e3c74b08
Sander Revenberg <sander@revenberg.info>
2021-02-13 15:32:31 +0100

    .

--- roles/docker_chromium/files/myscreen.sh
+++ roles/docker_chromium/files/myscreen.sh
@@ -9,8 +9,9 @@ export DISPLAY=:0.0
 #sed -i 's/"exited_cleanly":false/"exited_cleanly":true/' /home/pi/.config/chromium/Default/Preferences
 #sed -i 's/"exit_type":"Crashed"/"exit_type":"Normal"/' /home/pi/.config/chromium/Default/Preferences
 
-chromium-browser --force-device-scale-factor=0.5 --noerrdialogs --disable-infobars --window-size=480,320 --kiosk http://127.0.0.1:1880/ui  &
+chromium-browser --force-device-scale-factor=0.5 --noerrdialogs --disable-infobars --window-size=480,320 http://127.0.0.1:1880/ui  &
 
+#--kiosk
 while true; do
    xdotool keydown ctrl+Tab; xdotool keyup ctrl+Tab;
    sleep 10
