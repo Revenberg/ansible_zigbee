565a1213f287a322a42b9219db90978e2ee383bf
Sander Revenberg <sander@revenberg.info>
2021-02-20 19:29:43 +0100

    ble

--- roles/docker_zigbee/files/data/node-red/entrypoint.sh
+++ roles/docker_zigbee/files/data/node-red/entrypoint.sh
@@ -5,7 +5,7 @@ DIR=`pwd`
 cd /data
 npm install
 
-sudo apt-get install -y libbluetooth-dev 
+sudo apt-get install -y libbluetooth-dev libudev-dev pi-bluetooth
 
 cd "$DIR"
 npm start -- --userDir /data
